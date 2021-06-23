ed1f3e17ca75010152c7ff805b55d3a2e807acb2
Jeffery Abbott <jeff@jefferyabbott.com>
2021-06-22 20:07:09 -0700

    Add bootstrap to display images in grid

--- src/public/client.js
+++ src/public/client.js
@@ -15,35 +15,45 @@ const updateStore = (store, newState) => {
 const render = async (root, state) => {
     root.innerHTML = App(state);
     
-    
     // show images
+    // if (Object.keys(state.roverData).length !== 0) {
+    //     const imageContainer = document.getElementById('rover_images');
+    //     state.roverData.forEach((r) => {
+    //         const imgDiv = document.createElement('div');
+    //         const imgElement = document.createElement('img');
+    //         imgElement.src = r.img_src;
+    //         imgElement.classList.add('roverImg');
+    //         const camera = document.createElement('p');
+    //         camera.textContent = r.camera.full_name
+
+    //         imgDiv.appendChild(imgElement)
+    //         imgDiv.appendChild(camera)
+    //         imageContainer.appendChild(imgDiv);
+    //     });
+    // }
+    
+}
+
+const showImages = (state) => {
+    
     if (Object.keys(state.roverData).length !== 0) {
-        const imageContainer = document.getElementById('rover_images');
-        state.roverData.forEach((r) => {
-            const imgDiv = document.createElement('div');
-            const imgElement = document.createElement('img');
-            imgElement.src = r.img_src;
-            imgElement.classList.add('roverImg');
-            const camera = document.createElement('p');
-            camera.textContent = r.camera.full_name
+        let imageContent = `<div class="container">
+        <div class="row">
 
-            imgDiv.appendChild(imgElement)
-            imgDiv.appendChild(camera)
-            imageContainer.appendChild(imgDiv);
-        });
+      `
+      state.roverData.forEach((r) => {
+        imageContent += `<div class="col-lg-4">
+        <img src="${r.img_src}" class="roverImg" alt="...">
+      </div>`
+      })
+      imageContent +=  `
+    </div></div>
+        `
+        return imageContent
+    } else {
+        return `<div>Please select a rover</div>`
     }
 
-    if (state.selectedRover) {
-        const allRovers = document.querySelectorAll('.roverLabel')
-        allRovers.forEach((r) => {
-            r.classList.remove('seletedRover');
-        });
-        const roverLabel = document.getElementById(state.selectedRover)
-        roverLabel.classList.add('selectedRover')
-    }
-    
-    
-    
 }
 
 const displayRoverData = (rover) => {
@@ -56,22 +66,16 @@ const displayRoverData = (rover) => {
     store.selectedRover = rover 
     loadRoverImages(rover)
 
-    
-
-    const roverLabel = document.getElementById(rover)
-    roverLabel.classList.add('selectedRover')
-    
 
 }
 
 // generate menu
 const roverLinks = (rovers) => {
-    htmlText = '<div class="menu"><ul>'
+    let roverList = ''
     rovers.forEach((r) => {
-        htmlText += `<li onclick="displayRoverData('${r}')" class="roverLabel" id=${r}>${r}</li>`
+        roverList += `<li onclick="displayRoverData('${r}')"><p class="dropdown-item">${r}</p></li>`
     })
-    htmlText += '</ul></div>'
-    return htmlText
+    return roverList
 }
 
 // create content
@@ -80,14 +84,31 @@ const App = (state) => {
 
     return `
         <header>
-            <h1>Mars Dashboard</h1>
-            <hr/>
-            <div>${roverLinks(rovers)}</div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Mars Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Rovers
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+          ${roverLinks(rovers)}
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
         </header>
         <hr/>
         <main>
-            <div id="rover_display"></div>
-            <div id="rover_images"></div>
+            ${showImages(state)}
         </main>
     `
 }
@@ -98,12 +119,6 @@ window.addEventListener('load', () => {
 })
 
 // ------------------------------------------------------  COMPONENTS
-
-const ShowImages = (photos) => {
-    if (photos.length !== 0) {
-        console.log(photos)
-    }
-}
 
 
 
