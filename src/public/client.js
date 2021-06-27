5dea8d28badb87b002336d2c1e847dcc715c963b
Jeffery Abbott <jeff@jefferyabbott.com>
2021-06-27 11:34:06 -0700

    Add prettier and move component

--- src/public/client.js
+++ src/public/client.js
@@ -1,89 +1,56 @@
+// let store = {
+//     selectedRover: '',
+//     roverData: {},
+//     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+// }
 
-
-let store = {
-    selectedRover: '',
-    roverData: {},
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+let store = Immutable.Map({
+  selectedRover: '',
+  roverData: {},
+  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+});
 
 // add our markup to the page
 const root = document.getElementById('root');
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState);
-    render(root, store);
-}
+// const updateStore = (store, newState) => {
+//     store = Object.assign(store, newState);
+//     render(root, store);
+// }
 
+const updateStore = (newState) => {
+  const newStore = store.merge(store, newState);
+  return newStore;
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state);    
-}
-
-const showImages = (roverData) => {
-    if (Object.keys(roverData).length !== 0) {
-        let imageContent = `<div class="container">
-        <div>
-            <h1>${roverData[0].rover.name}</h1>
-            <table class="table">
-                <tr>
-                    <td>Launch date</td>
-                    <td>${roverData[0].rover.launch_date}</td>
-                </tr>
-                <tr>
-                    <td>Landing date</td>
-                    <td>${roverData[0].rover.landing_date}</td>
-                </tr>
-                <tr>
-                    <td>Photo date</td>
-                    <td>${roverData[0].earth_date}</td>
-                </tr>
-                <tr>
-                    <td>Status</td>
-                    <td>${roverData[0].rover.status}</td>
-                </tr>
-            </table>
-        </div>
-        <div class="row">`;
-
-      roverData.forEach((r) => {
-        imageContent += `<div class="col-lg-4 align-self-end">
-        <img src="${r.img_src}" class="roverImg mx-auto d-block" alt="...">
-        <p class="cameraName">${r.camera.full_name}</p>
-      </div>`
-      })
-      imageContent +=  `
-    </div></div>`;
-        return imageContent;
-    } else {
-        return `<div>Please select a rover</div>`;
-    }
-}
+  root.innerHTML = App(state);
+};
 
 const displayRoverData = (rover) => {
-    const allRovers = document.querySelectorAll('.roverLabel')
-    allRovers.forEach((r) => {
-        r.classList.remove('seletedRover');
-    });
+  const allRovers = document.querySelectorAll('.roverLabel');
+  allRovers.forEach((r) => {
+    r.classList.remove('seletedRover');
+  });
 
-    store.selectedRover = rover;
-    loadRoverImages(rover);
-}
+  store.selectedRover = rover;
+  loadRoverImages(rover);
+};
 
 // generate menu
 const roverLinks = (rovers) => {
-    let roverList = '';
-    rovers.forEach((r) => {
-        roverList += `<li onclick="displayRoverData('${r}')"><p class="dropdown-item">${r}</p></li>`;
-    })
-    return roverList;
-}
-
+  let roverList = '';
+  rovers.forEach((r) => {
+    roverList += `<li onclick="displayRoverData('${r}')"><p class="dropdown-item">${r}</p></li>`;
+  });
+  return roverList;
+};
 
 // create content
 const App = (state) => {
-    let { rovers, roverData } = state;
+  let { rovers, roverData } = state;
 
-    return `
+  return `
         <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
@@ -107,33 +74,72 @@ const App = (state) => {
         <main>
             ${showImages(roverData)}
         </main>`;
-}
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store);
-})
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
+const showImages = (roverData) => {
+  if (Object.keys(roverData).length !== 0) {
+    let imageContent = `<div class="container">
+          <div>
+              <h1>${roverData[0].rover.name}</h1>
+              <table class="table">
+                  <tr>
+                      <td>Launch date</td>
+                      <td>${roverData[0].rover.launch_date}</td>
+                  </tr>
+                  <tr>
+                      <td>Landing date</td>
+                      <td>${roverData[0].rover.landing_date}</td>
+                  </tr>
+                  <tr>
+                      <td>Photo date</td>
+                      <td>${roverData[0].earth_date}</td>
+                  </tr>
+                  <tr>
+                      <td>Status</td>
+                      <td>${roverData[0].rover.status}</td>
+                  </tr>
+              </table>
+          </div>
+          <div class="row">`;
 
+    roverData.forEach((r) => {
+      imageContent += `<div class="col-lg-4 align-self-end">
+          <img src="${r.img_src}" class="roverImg mx-auto d-block" alt="...">
+          <p class="cameraName">${r.camera.full_name}</p>
+        </div>`;
+    });
+    imageContent += `
+      </div></div>`;
+    return imageContent;
+  } else {
+    return `<div>Please select a rover</div>`;
+  }
+};
 
 // ------------------------------------------------------  API CALLS
 
 const loadRoverImages = (rover) => {
-    let url = new URL(`http://localhost:3000/nasa?rover=${rover}`);
-    fetch(url)
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw new Error('Oops! Something went wrong! Please try again.');
-            }
-        }).then(data => {
-            const roverData = data.roverData.latest_photos
-            updateStore(store, {...store, roverData} );
-        }).catch(error => {
-            alert(error.message);
-        });
-}
-
+  let url = new URL(`http://localhost:3000/nasa?rover=${rover}`);
+  fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Error while requesting rover data');
+      }
+    })
+    .then((data) => {
+      const roverData = data.roverData.latest_photos;
+      updateStore(store, { ...store, roverData });
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
 
 app.get('/nasa', async (req, res) => {
-    const { rover } = req.query
-    try {
-        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({roverData})
-    } catch (error) {
-        console.log(error)
-    }
+  const { rover } = req.query;
+  try {
+    let roverData = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send({ roverData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
-app.listen(port, () => console.log(`Mars Dashboard server listening on port ${port}!`))
+app.listen(port, () =>
+  console.log(`Mars Dashboard server listening on port ${port}!`)
+);
